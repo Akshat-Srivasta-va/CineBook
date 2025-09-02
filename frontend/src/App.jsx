7025e5ee5dfe02687523865e5a2ee14e2fdef700
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-09-02 22:56:22 +0530

    inside pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,13 @@ import Favourite from './pages/Favourite'
 import Footer from './components/Footer'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
+import Layout from './pages/admin/LayOut'
+import DashBoard from './pages/admin/DashBoard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
+
+
 
 const App = () => {
 
@@ -25,6 +32,12 @@ const isAdminRoute = useLocation().pathname.startsWith('/admin');
   <Route path='/movies/:id/:date' element={<SeatLayout />}/>
   <Route path='/my-bookings' element={<MyBooking/>}/>
   <Route path='/favourite' element={<Favourite/>}/>
+  <Route path='/admin/*' element={<Layout />}>
+      <Route index  element={<DashBoard />}/>
+      <Route index path='add-shows' element={<AddShows />}/>
+      <Route index path='list-shows' element={<ListShows />}/>
+      <Route index path='list-bookings' element={<ListBookings />}/>
+  </Route> 
 </Routes>
  {!isAdminRoute && <Footer />}
     </>
---
+++ frontend/src/pages/admin/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>LayOut</div>
+  )
+}
+
+export default Layout
