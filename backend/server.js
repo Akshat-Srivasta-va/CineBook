ecc008a09ed217adea18c17ae514fb902079015a
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-09-06 17:14:35 +0530

    env is added

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,22 @@
-import express from express;
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
 
+const app = express()
+const port = 3000;
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+
+// API Routes
+app.get('/', (req, res)=> res.send('Server is Live'))
+
+app.listen(port, ()=> console.log(`Server listening at https://localhost:${port}`)
+)
+
+
+
+// username -> srivastavaakshat52_db_user   password -> TdMCOoFDuBRcXiXM
+
+// mongodb+srv://srivastavaakshat52_db_user:TdMCOoFDuBRcXiXM@cluster0.a7l59vw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
