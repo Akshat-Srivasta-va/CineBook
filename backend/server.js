415f3c3c4d499bcbad68ef9ceb9f7f2142b0006a
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-09-06 17:20:35 +0530

    foder config is made and db.js is added

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,4 @@ app.listen(port, ()=> console.log(`Server listening at https://localhost:${port}
 
 // username -> srivastavaakshat52_db_user   password -> TdMCOoFDuBRcXiXM
 
-// mongodb+srv://srivastavaakshat52_db_user:TdMCOoFDuBRcXiXM@cluster0.a7l59vw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// 
