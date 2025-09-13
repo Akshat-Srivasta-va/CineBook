bb90247d9262de84319296e9be2032f1ad6871fd
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-09-13 23:16:40 +0530

    added tmdb api

--- backend/server/controllers/showController.js
+++ backend/server/controllers/showController.js
@@ -2,8 +2,15 @@
 
 export const getNowPlayingMovies = async(req, res) => {
     try {
+const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+    headers: {Autherization : `Bearer ${ProcessingInstruction.env.TMDB_API_KEY }`}
+}) 
+
+const movies = data.results;
+res.json({success: true, movies: movies})
 
     } catch(error) {
-        
+       console.log(error);
+       res.json({success: false, message: error.message})
     }
 }
