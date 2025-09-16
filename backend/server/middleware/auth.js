d09d2cc4d6175f15d09d08e0eb0aa39736379da4
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-09-17 00:20:20 +0530

    auth.js is added

---
+++ backend/server/middleware/auth.js
@@ -0,0 +1,16 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectAdmin = async (req, res, next)=>{
+    try {
+        const { userId } = req.auth();
+
+        const usser = await clerkClient.users.getUser(userId)
+
+        if(userId.privateMetadata.role != 'admin') {
+           return res.json({success: false, message: "not authorized"}) 
+        }
+        next();
+    } catch (error) {
+        return res.json({ success: false, message: "not authorized" }); 
+}
+}
