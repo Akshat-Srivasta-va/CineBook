3ac9fe7a3e5c9d2762f7a5136367fcbdf0b215a5
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-09-18 23:13:02 +0530

    API to get dashboard data

--- backend/server/controllers/adminController.js
+++ backend/server/controllers/adminController.js
@@ -1,5 +1,15 @@
-// API to check if user is admin
+import Booking from "../models/Booking.js"
 
+// API to check if user is admin
 export const isAdmin = async (req, res) =>{
-    res.json({success: true})
+    res.json({success: true, isAdmin: true})
+}
+
+// API to get dashboard data
+export const getDashboardData = async(req, res) =>{
+    try {
+        const bookings = await Booking.find({isPaid: true})
+    } catch (error) {
+ 
+    }
 }
