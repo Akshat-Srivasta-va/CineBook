27008dd78c4d6dac1858a67ecd0d285e2384531c
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-10-07 02:36:13 +0530

    comee

--- backend/server/routes/showRoutes.js
+++ backend/server/routes/showRoutes.js
@@ -5,9 +5,9 @@ import { protectAdmin } from '../middleware/auth.js';
 
 const showRouter = express.Router();
 
-showRouter.get("/", (req, res) => {
-  res.json({ message: "Show route working" });
-});
+// showRouter.get("/", (req, res) => {
+//   res.json({ message: "Show route working" });
+// });
 
 showRouter.get('/now-playing',protectAdmin, getNowPlayingMovies)
 showRouter.post('/add',protectAdmin, addShow)
